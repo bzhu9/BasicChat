6501509816409cca5ebf20ad67f1c598ea9bf5de
Brian Zhu <bzhu@parktudor.org>
2020-06-25 13:32:13 -0400

    Initial Commit

---
+++ BasicChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicChat
+//
+//  Created by Brian Zhu on 6/25/20.
+//  Copyright © 2020 Brian Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
