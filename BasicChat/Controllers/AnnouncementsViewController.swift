ffedf1c7de449ab687dee1577ea7b1757bf772b6
Brian Zhu <bzhu@parktudor.org>
2020-09-17 22:17:58 -0400

    Self sizing announcements cells

--- BasicChat/Controllers/AnnouncementsViewController.swift
+++ BasicChat/Controllers/AnnouncementsViewController.swift
@@ -13,36 +13,26 @@ import UIKit
 class AnnouncementsViewController: UIViewController {
     
     private var announcements = [Announcement]()
-    
-    private let tableView: UITableView = {
-        let table = UITableView()
-        table.tableFooterView = UIView(frame: .zero)
-        table.register(AnnouncementTableViewCell.self, forCellReuseIdentifier: AnnouncementTableViewCell.identifier)
-        return table
-    }()
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let composeButton = UIBarButtonItem(barButtonSystemItem: .add,
                                             target: self,
                                             action: #selector(didTapComposeButton))
-        view.addSubview(tableView)
         navigationItem.rightBarButtonItems = [composeButton]
         startListeningForAnnouncements()
-        tableView.delegate = self
+//        tableView.delegate = self
         tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     @objc private func didTapComposeButton(){
-        AnnouncementsDatabaseManager.shared.createNewAnnouncement(with: Announcement(authorName: "Brian Zhu", authorEmail: "bzhu", title: "My first post", description: "wow look this is my first post!", organization: "Test", photoURLS: ["testurl"], comments: [Comment(senderName: "Kyle Xu", senderEmail: "kxu", commentText: "Wow this ia great post!")]), completion: {_ in
+        AnnouncementsDatabaseManager.shared.createNewAnnouncement(with: Announcement(authorName: "Brian Zhu", authorEmail: "bzhu", title: "My first post", description: "wow look this is my first post! wow look this is my first post! wow look this is my first post! wow look this is my first post! wow look this is my first post!", organization: "Test", photoURLS: ["testurl"], comments: [Comment(senderName: "Kyle Xu", senderEmail: "kxu", commentText: "Wow this ia great post!")]), completion: {_ in
             print("Success")
         })
     }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        tableView.frame = view.bounds
-    }    
+    
     private func startListeningForAnnouncements(){
         print("Starting announcements fetch...")
         AnnouncementsDatabaseManager.shared.getAllAnouncements({ [weak self] result in
@@ -62,20 +52,20 @@ class AnnouncementsViewController: UIViewController {
     }
 }
 
-extension AnnouncementsViewController: UITableViewDelegate, UITableViewDataSource {
+extension AnnouncementsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return announcements.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let model = announcements[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: AnnouncementTableViewCell.identifier, for: indexPath) as! AnnouncementTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnnouncementTableViewCell", for: indexPath) as! AnnouncementTableViewCell
         cell.configure(with: model)
         return cell
     }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 300
-    }
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 300
+//    }
     
     
 }
--- BasicChat/Views/AnnouncementTableViewCell.swift
+++ BasicChat/Views/AnnouncementTableViewCell.swift
@@ -10,41 +10,8 @@ import UIKit
 
 class AnnouncementTableViewCell: UITableViewCell {
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 21, weight: .bold)
-//        label.text = "El Primo Title"
-        label.textAlignment = .center
-        return label
-    }()
-    private let descriptionLabel: UILabel = {
-        let label = UILabel()
-//        label.text = "ELLLLLLL PRIMOOOOOOOO ELLLLLLL PRIMOOOOOOOO ELLLLLLL PRIMOOOOOOOO ELLLLLLL PRIMOOOOOOOO ELLLLLLL PRIMOOOOOOOO"
-        label.font = .systemFont(ofSize: 19, weight: .regular)
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        //label.lineBreakMode = NSLineBreakMode.byWordWrapping
-        return label
-    }()
-    
-    static let identifier = "AnnouncementTableViewCell"
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(descriptionLabel)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        titleLabel.frame = CGRect(x: 0, y: 10, width: contentView.width, height: 20)
-        descriptionLabel.frame = CGRect(x: 10, y: titleLabel.bottom+10, width: contentView.width, height: (contentView.height-10)/2)
-        
-    }
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     public func configure (with model: Announcement) {
         titleLabel.text = model.title
